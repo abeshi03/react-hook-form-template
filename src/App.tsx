5ea9c08ae4a074673a968177389fafc043fa3dc4
abeshi03 <k_913411@yahoo.co.jp>
2021-12-04 22:17:25 +0900

    ログイン確認完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import React, { VFC } from 'react';
-import { useState } from "react";
-import { auth } from "../firebase";
+import React, {ChangeEvent, FormEvent, useState, VFC} from 'react';
+import { auth } from "./firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
+
 const App: VFC = () => {
+
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value)
+  }
+
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password).then(() => { console.log("成功")})
+  }
+
   return (
-    <div className="App">
-      テスト
-    </div>
+    <form className="App" onSubmit={handleSubmit}>
+      <p>メールアドレス</p>
+      <input
+        type="text"
+        value={email}
+        onChange={onChangeEmail}
+      />
+      <p>パスワード</p>
+      <input
+        type="password"
+        value={password}
+        onChange={onChangePassword}
+      />
+      <button type="submit">会員登録</button>
+    </form>
   );
 }
 
