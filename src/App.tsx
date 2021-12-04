6756a14b4e7426eeaa919134bfd88acde1db2ec3
abeshi03 <k_913411@yahoo.co.jp>
2021-12-04 21:08:56 +0900

    install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { VFC } from 'react';
+import { useState } from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-function App() {
+const App: VFC = () => {
   return (
     <div className="App">
       テスト
