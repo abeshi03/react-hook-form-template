ba8092d09124dc12310a2bd76becc86148205e56
abeshi03 <k_913411@yahoo.co.jp>
2021-12-04 21:04:28 +0900

    setting firebase auth

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -12,4 +12,5 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app)
+export const auth = getAuth();
+
