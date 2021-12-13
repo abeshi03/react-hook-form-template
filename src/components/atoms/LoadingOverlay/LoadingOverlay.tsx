0893c7971b6ccd4c9e6248f7717fdb6997091451
abeshi03 <k_913411@yahoo.co.jp>
2021-12-14 08:50:39 +0900

    LoadingOverlayの条件分岐を変更

--- src/components/atoms/LoadingOverlay/LoadingOverlay.tsx
+++ src/components/atoms/LoadingOverlay/LoadingOverlay.tsx
@@ -8,16 +8,22 @@ type Props = {
   isLoadingOverlay: boolean;
 }
 
+const displayLoadingOverlay = (displayFlag: boolean) => {
+  if (displayFlag) {
+    return (
+      <div className={styles.loadingOverlay}>
+        <div className={styles.loadingOverlay__loadingIndicator}></div>
+      </div>
+    )
+  }
+}
+
 /* eslint-disable-next-line react/display-name */
 export const LoadingOverlay: VFC<Props> = memo(({ isLoadingOverlay }) => {
 
   return (
     <>
-      { isLoadingOverlay &&
-        <div className={styles.loadingOverlay}>
-          <div className={styles.loadingOverlay__loadingIndicator}></div>
-        </div>
-      }
+      {displayLoadingOverlay(isLoadingOverlay)}
     </>
   );
 });
--- src/components/organism/controlGroup/SignUpControlGroup/SignUpControlGroup.tsx
+++ src/components/organism/controlGroup/SignUpControlGroup/SignUpControlGroup.tsx
@@ -1,5 +1,5 @@
 // - ライブラリー ========================================================================================================
-import React, {memo, VFC, useContext, useState} from "react";
+import React, { memo, VFC, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../../firebase";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -9,6 +9,7 @@ import styles from "./SignUpControlGroup.module.scss";
 
 // - 子コンポーネント =====================================================================================================
 import { InputField } from "../../../atoms/InputField/InputField";
+import { LoadingOverlay } from "../../../atoms/LoadingOverlay/LoadingOverlay";
 
 // - バリデーション =======================================================================================================
 import { signUpValidations } from "../../../../config/validations/signUpValidations";
@@ -29,56 +30,64 @@ export const SignUpControlGroup: VFC = memo(() => {
   const { register, handleSubmit, formState: { errors } } = useForm<SignUpInputValues>();
 
   const [ isDisabled, setIsDisabled ] = useState(false);
+  const [ isLoadingOverlay, setIsLoadingOverlay ] = useState(false);
 
   const onSubmit: SubmitHandler<SignUpInputValues> = (inputValue) => {
 
     setIsDisabled(true);
+    setIsLoadingOverlay(true);
 
     createUserWithEmailAndPassword(auth, inputValue.email, inputValue.password)
       .then(() => console.log("成功"))
       .catch((error) => console.log(error))
       .finally(() => {
         setIsDisabled(false);
+        setIsLoadingOverlay(false);
       })
   }
 
 
   return (
-    <form className={styles.signInControlGroup} onSubmit={handleSubmit(onSubmit)}>
+    <>
+      <form className={styles.signInControlGroup} onSubmit={handleSubmit(onSubmit)}>
 
-      <div className={styles.inputContainer}>
-        <InputField
-          type="text"
-          required={signUpValidations.email.required}
-          label="メールアドレス"
-          placeholder="メールアドレスを入力してください"
-          inputProps={register("email",{
-            required: signUpValidations.email.required,
-            pattern: signUpValidations.email.regexp
-          })}
-          autoComplete="email"
-        />
-        {errors.email && emailErrorMessages(errors.email)}
-      </div>
+        <div className={styles.inputContainer}>
+          <InputField
+            type="text"
+            required={signUpValidations.email.required}
+            label="メールアドレス"
+            placeholder="メールアドレスを入力してください"
+            inputProps={register("email",{
+              required: signUpValidations.email.required,
+              pattern: signUpValidations.email.regexp
+            })}
+            autoComplete="email"
+          />
+          {errors.email && emailErrorMessages(errors.email)}
+        </div>
 
-      <div className={styles.inputContainer}>
-        <InputField
-          type="password"
-          required={signUpValidations.password.required}
-          label="パスワード"
-          guidance={`※パスワードは最低${signUpValidations.password.minLength}文字以上で入力してください`}
-          placeholder="パスワードを入力してください"
-          inputProps={register("password", {
-            required: signUpValidations.password.required,
-            minLength: signUpValidations.password.minLength,
-            maxLength: signUpValidations.password.maxLength
-          })}
-          autoComplete="new-password"
-        />
-        {errors.password && passwordErrorMessages(errors.password)}
-      </div>
+        <div className={styles.inputContainer}>
+          <InputField
+            type="password"
+            required={signUpValidations.password.required}
+            label="パスワード"
+            guidance={`※パスワードは最低${signUpValidations.password.minLength}文字以上で入力してください`}
+            placeholder="パスワードを入力してください"
+            inputProps={register("password", {
+              required: signUpValidations.password.required,
+              minLength: signUpValidations.password.minLength,
+              maxLength: signUpValidations.password.maxLength
+            })}
+            autoComplete="new-password"
+          />
+          {errors.password && passwordErrorMessages(errors.password)}
+        </div>
 
-      <button type="submit" disabled={isDisabled}>送信</button>
-    </form>
+        <button type="submit" disabled={isDisabled}>送信</button>
+
+      </form>
+
+      <LoadingOverlay isLoadingOverlay={isLoadingOverlay}/>
+    </>
   );
 });
