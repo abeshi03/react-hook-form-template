cf34518a9b2ce67c8bd06e593989b9c80081ef53
abeshi03 <k_913411@yahoo.co.jp>
2021-12-19 12:55:27 +0900

    routerを一時的に設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // - フレームワーク =======================================================================================================
 import React, { VFC } from 'react';
-
-
+import { Link } from "react-router-dom";
 
 const App: VFC = () => {
 
   return (
     <>
       <div>トップページです</div>
+      <Link to="/sign_up">会員登録</Link>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import "./assets/global/errorMessage.scss";
 import App from './App';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SignUp } from "./components/pages/SignUp/SignUp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="/sign_up" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
