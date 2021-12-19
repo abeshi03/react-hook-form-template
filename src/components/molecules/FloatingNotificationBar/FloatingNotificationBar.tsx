c3352e18b18eac97fe5f227fe8b0dcb146eff232
abeshi03 <k_913411@yahoo.co.jp>
2021-12-19 11:56:21 +0900

    FloatingNotificationBar: 主要マークアップ追加

---
+++ src/components/molecules/FloatingNotificationBar/FloatingNotificationBar.tsx
@@ -0,0 +1,52 @@
+// - フレームワーク =======================================================================================================
+import React, { memo, VFC } from "react";
+
+// - アセット ============================================================================================================
+import styles from "./FloatingNotificationBar.module.scss";
+import { ErrorIcon } from "../../../assets/icons/ErrorIcon";
+import { InfoIcon } from "../../../assets/icons/InfoIcon";
+import { WarningIcon } from "../../../assets/icons/WarningIcon";
+import { SuccessIcon } from "../../../assets/icons/SuccessIcon";
+
+// - ルーティング =========================================================================================================
+
+type AlertType = "SUCCESS" | "ERROR" | "WARNING" | "INFO"
+
+type Props = {
+  type: AlertType;
+  message: string;
+}
+
+const variationModifierCSS_Class = (type: AlertType): string => {
+  switch (type) {
+    case "ERROR": return styles.floatingNotificationBar__error;
+    case "WARNING": return styles.floatingNotificationBar__warning;
+    case "INFO" : return styles.floatingNotificationBar__info;
+    case "SUCCESS" : return styles.floatingNotificationBar__success;
+  }
+}
+
+
+const getIcon = (type: AlertType): JSX.Element => {
+  switch (type) {
+    case "ERROR": return <ErrorIcon/>;
+    case "WARNING": return <WarningIcon/>;
+    case "INFO" : return <InfoIcon/>;
+    case "SUCCESS" : return  <SuccessIcon/>;
+  }
+}
+
+
+export const FloatingNotificationBar: VFC<Props> = memo((props) => {
+  const { type, message } = props;
+
+  return (
+    <div className={`${styles.floatingNotificationBar} ${variationModifierCSS_Class(type)}`}>
+      <div className={styles.icon}>
+        { getIcon(type) }
+      </div>
+      <p className={styles.message}>{ message }</p>
+      <div className={styles.closeButton} role="button">{'×'}</div>
+    </div>
+  );
+});
