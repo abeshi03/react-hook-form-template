f91f9a9b09c3063994f0a50c1d3f01abb580eea5
abeshi03 <k_913411@yahoo.co.jp>
2021-12-30 14:21:39 +0900

    画像アップロードアイコン追加

---
+++ src/assets/icons/ImageAddingIcon.tsx
@@ -0,0 +1,10 @@
+// - フレームワーク =======================================================================================================
+import React, { memo, VFC } from "react";
+
+export const ImageAddingIcon: VFC = memo(() => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21">
+      <path d="M2 0a2 2 0 00-2 2v14c0 1.1.9 2 2 2h9.1l-.1-1c0-.7.1-1.4.4-2H2l3.5-4.5 2.5 3L11.5 9l2.2 3c1-.6 2.1-1 3.3-1l1 .1V2a2 2 0 00-2-2H2m14 13v3h-3v2h3v3h2v-3h3v-2h-3v-3h-2z" />
+    </svg>
+  );
+});
