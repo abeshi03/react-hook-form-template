5f2cfc39d40ba0d8f0a9f40892dc6feaf4317940
abeshi03 <k_913411@yahoo.co.jp>
2022-01-06 19:12:05 +0900

    セレクトフィールド: 中間実装完了

---
+++ src/components/atoms/control/SelectField/SelectField.tsx
@@ -0,0 +1,61 @@
+// - ライブラリ ==========================================================================================================
+import React, { memo, VFC } from "react";
+
+// - アセット ============================================================================================================
+import styles from "./SelectField.module.scss";
+
+// - ルーティング =========================================================================================================
+
+export type SelectFieldType = {
+  readonly key: string;
+  readonly label: string;
+  readonly initiallySelected?: boolean;
+}
+
+type Props = {
+  label?: string;
+  required: boolean;
+  guidance?: string;
+  disabled?: boolean;
+  selectOptions: SelectFieldType[];
+  emptySelect?: boolean;
+  inputProps: React.HTMLAttributes<HTMLSelectElement>;
+}
+
+/* eslint-disable-next-line react/display-name */
+export const SelectField: VFC<Props> = memo((props) => {
+
+  const {
+    label,
+    required,
+    guidance,
+    disabled,
+    selectOptions,
+    emptySelect = false,
+    inputProps
+  } = props;
+
+  return (
+    <div className={styles.selectFieldContainer}>
+      <div className={styles.labelAndRequiredBadge}>
+        {label && <label htmlFor={label} className={styles.label}>{label}</label>}
+        {required && <span className={styles.requiredBadge}>必須</span>}
+      </div>
+
+      <select
+        { ...inputProps }
+        className={styles.selectField}
+        disabled={disabled}
+      >
+        {emptySelect && <option value="">未選択</option>}
+        {selectOptions.map((selectOption: SelectFieldType) => (
+          <option
+            key={selectOption.key}
+            value={selectOption.key}
+          >{selectOption.label}</option>
+        ))}
+      </select>
+      {guidance && <p className={styles.guidance}>{guidance}</p>}
+    </div>
+  );
+});
