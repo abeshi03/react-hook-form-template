3712c7ce3be3a2a67d326b012adbc5229ac5dfdc
abeshi03 <k_913411@yahoo.co.jp>
2022-01-16 12:16:56 +0900

    Checkbox: コンポーネントファイル設定完了

---
+++ src/components/molecules/control/Checkbox/Checkbox.tsx
@@ -0,0 +1,16 @@
+// - フレームワーク =======================================================================================================
+import React, { memo, VFC } from "react";
+
+// - アセット ============================================================================================================
+import styles from "./Checkbox.module.scss"
+
+
+/* eslint-disable-next-line react/display-name */
+export const Checkbox: VFC = memo((props) => {
+
+  // const {$END$} = props;
+
+  return (
+    null
+  );
+});
